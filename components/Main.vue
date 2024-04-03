73a8e7987990cde0ad7391c9c4f7caf48d88ff9d
Kasun Madushan <kasunsudarshana1@gmail.com>
2024-04-04 03:56:45 +0530

    remove nuxt ui

--- components/Main.vue
+++ components/Main.vue
@@ -1,35 +1,10 @@
 <template>
-    <UContainer class="min-h-screen flex items-center">
-        <UPopover :popper="{ placement: 'bottom-start' }">
-            <UButton icon="i-heroicons-calendar-days-20-solid" :label="format(date, 'd MMM, yyy')" />
-
-            <template #panel="{ close }">
-            <DatePicker v-model="date" is-required @close="close" />
-            </template>
-        </UPopover>
-        <UCard class="flex-1"
-            :ui="{ background: 'bg-gray-50 dark:bg-gray-800/50', ring: 'ring-1 ring-gray-300 dark:ring-gray-700', divide: 'divide-y divide-gray-300 dark:divide-gray-700', header: { base: 'font-bold' } }">
-            <template #header>
-                Test content
-            </template>
-
-            <p class="text-gray-500 dark:text-gray-400">
-                Can I still push, is it okay even now?
-            </p>
-
-        </UCard>
-    </UContainer>
+    <div class="min-h-screen flex items-center"></div>
 </template>
 
 <script setup>
-import { format } from 'date-fns'
-
-const date = ref(new Date())
-
 </script>
 
 <style>
-body {
-    @apply antialiased font-sans text-gray-700 dark:text-gray-200 bg-white dark:bg-gray-900;
-}
+
 </style>
