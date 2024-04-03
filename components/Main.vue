83abfdfd00d2cdd148d6e101448e05f1125f5d1b
Kasun Madushan <kasunsudarshana1@gmail.com>
2024-04-04 04:01:01 +0530

    initial ui breakdown

--- components/Main.vue
+++ components/Main.vue
@@ -1,5 +1,10 @@
 <template>
-    <div class="min-h-screen flex items-center"></div>
+    <div class="min-h-screen flex items-center">
+        <p>Date picker</p>
+        <p>Category</p>
+        <p>Color mode switch</p>
+        <p class="text-3xl font-bold underline">Main content</p>
+    </div>
 </template>
 
 <script setup>
