cb60d03d4755cc74cc5b9ca5da69dd905e7f7039
Kasun Madushan <kasunsudarshana1@gmail.com>
2024-04-04 03:36:36 +0530

    test push

--- components/Main.vue
+++ components/Main.vue
@@ -10,7 +10,7 @@
         <UCard class="flex-1"
             :ui="{ background: 'bg-gray-50 dark:bg-gray-800/50', ring: 'ring-1 ring-gray-300 dark:ring-gray-700', divide: 'divide-y divide-gray-300 dark:divide-gray-700', header: { base: 'font-bold' } }">
             <template #header>
-                Welcome to the playground!
+                Test content
             </template>
 
             <p class="text-gray-500 dark:text-gray-400">
