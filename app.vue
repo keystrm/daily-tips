afeb1ea3409d03290b0a1b53f214b1356273006d
Kasun Madushan <kasunsudarshana1@gmail.com>
2024-04-06 23:58:32 +0530

    remove filters for better user focus on the content

--- app.vue
+++ app.vue
@@ -1,33 +1,8 @@
 <template>
   <NuxtLayout>
     <div class="md:max-w-2xl">
-      <div class="flex flex-col md:flex-row gap-2 my-3 md:justify-between">
-        <div class="flex flex-col md:flex-row">
-          <Dropdown v-model="selectedCountry" :options="countries" filter optionLabel="name"
-            placeholder="Select a Country" class="w-full md:w-14rem">
-            <template #value="slotProps">
-              <div v-if="slotProps.value" class="flex align-items-center">
-                <img :alt="slotProps.value.label"
-                  src="https://primefaces.org/cdn/primevue/images/flag/flag_placeholder.png"
-                  :class="`mr-2 flag flag-${slotProps.value.code.toLowerCase()}`" style="width: 18px" />
-                <div>{{ slotProps.value.name }}</div>
-              </div>
-              <span v-else>
-                {{ slotProps.placeholder }}
-              </span>
-            </template>
-            <template #option="slotProps">
-              <div class="flex align-items-center">
-                <img :alt="slotProps.option.label"
-                  src="https://primefaces.org/cdn/primevue/images/flag/flag_placeholder.png"
-                  :class="`mr-2 flag flag-${slotProps.option.code.toLowerCase()}`" style="width: 18px" />
-                <div>{{ slotProps.option.name }}</div>
-              </div>
-            </template>
-          </Dropdown>
-          <Calendar v-model="buttondisplay" showIcon />
-        </div>
-
+      <div class="flex flex-col md:flex-row gap-2 my-3 md:justify-end">
+        <Button icon="pi pi-filter" aria-label="Filter" />
         <ColorModeSwitch />
       </div>
       <div class="">
