2730eb9a8f5088c504fc8750146ac45bfdf70745
Kasun Madushan <kasunsudarshana1@gmail.com>
2024-04-06 23:57:49 +0530

    get card into a seperate component

--- app.vue
+++ app.vue
@@ -31,17 +31,7 @@
         <ColorModeSwitch />
       </div>
       <div class="">
-        <Card>
-          <template #title> Simple Card </template>
-          <template #content>
-            <p class="m-0">
-              Lorem ipsum dolor sit amet, consectetur adipisicing elit. Inventore sed consequuntur error repudiandae
-              numquam deserunt quisquam repellat libero asperiores earum nam nobis, culpa ratione quam perferendis esse,
-              cupiditate neque
-              quas!
-            </p>
-          </template>
-        </Card>
+        <ContentSlider />
       </div>
     </div>
 
