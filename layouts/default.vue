a9e099d137474764aaf37e23574ada3cf4d00ce1
Kasun Madushan <kasunsudarshana1@gmail.com>
2024-04-06 20:57:28 +0530

    use a grid template

--- layouts/default.vue
+++ layouts/default.vue
@@ -1,5 +1,5 @@
 <template>
-    <div class="grid grid-cols-2 min-h-screen bg-surface-0 dark:bg-surface-950 text-surface-950 dark:text-surface-50">
+    <div class="grid place-items-center min-h-screen bg-surface-0 dark:bg-surface-950 text-surface-950 dark:text-surface-50">
         <slot/>
     </div>
 
