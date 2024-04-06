a089fae58c1b17fbf1c8bf914ce0a822c3c6e60f
Kasun Madushan <kasunsudarshana1@gmail.com>
2024-04-06 19:55:09 +0530

    change some base theme to test

--- components/Main.vue
+++ components/Main.vue
@@ -1,28 +1,28 @@
 <template>
-    <div class="min-h-screen flex flex-col items-center justify-center space-y-4 bg-gray-100 dark:bg-gray-800 p-4">
+    <div class="min-h-screen dark:bg-surface-950 flex flex-col items-center justify-center space-y-4 p-4 text-surface-700 dark:text-surface-0">
         <!-- Placeholder for Date picker -->
-        <div class="bg-white dark:bg-gray-700 p-4 rounded-lg shadow">
-            <p class="text-gray-900 dark:text-gray-100">Date picker</p>
+        <div class="p-4 rounded-lg shadow">
+            <p class="">Date picker</p>
         </div>
 
         <!-- Placeholder for Category -->
-        <div class="bg-white dark:bg-gray-700 p-4 rounded-lg shadow">
-            <p class="text-gray-900 dark:text-gray-100">Category</p>
+        <div class="p-4 rounded-lg shadow">
+            <p class="">Category</p>
         </div>
 
         <!-- Placeholder for Color mode switch -->
-        <div class="bg-white dark:bg-gray-700 p-4 rounded-lg shadow flex justify-between items-center">
-            <p class="text-gray-900 dark:text-gray-100">Color mode switch</p>
+        <div class="p-4 rounded-lg shadow flex justify-between items-center">
+            <p class="">Color mode switch</p>
             <ColorModeSwitch/>
         </div>
 
         <!-- Main content -->
-        <div class="bg-white dark:bg-gray-700 p-4 rounded-lg shadow text-center">
-            <p class="text-3xl font-bold underline text-gray-900 dark:text-gray-100">Main content</p>
+        <div class="p-4 rounded-lg shadow text-center">
+            <p class="text-3xl font-bold underline ">Main content</p>
         </div>
 
         <!-- Additional components (e.g., Terminal, Card) -->
-        <div class="bg-white dark:bg-gray-700 p-4 rounded-lg shadow">
+        <div class="p-4 rounded-lg shadow">
             <!-- Card Component Placeholder -->
             <div class="mt-4">
                 <Card>
