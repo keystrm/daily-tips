f284e1212c834a9ec49c9f42fda0cffd134a15bd
Kasun Madushan <kasunsudarshana1@gmail.com>
2024-04-07 00:41:38 +0530

    add navigation buttons to switch between contents

--- components/ContentSlider.vue
+++ components/ContentSlider.vue
@@ -1,14 +1,20 @@
 <template>
-    <Card>
-        <template #title> Simple Card </template>
-        <template #content>
-            <p class="m-0">
-                Lorem ipsum dolor sit amet, consectetur adipisicing elit. Inventore sed consequuntur error repudiandae
-                numquam deserunt quisquam repellat libero asperiores earum nam nobis, culpa ratione quam perferendis
-                esse,
-                cupiditate neque
-                quas!
-            </p>
-        </template>
-    </Card>
+    <div class="flex">
+        <Button icon="pi pi-arrow-left"/>
+        <Card>
+            <template #title> Simple Card </template>
+            <template #content>
+                <p class="m-0">
+                    Lorem ipsum dolor sit amet, consectetur adipisicing elit. Inventore sed consequuntur error
+                    repudiandae
+                    numquam deserunt quisquam repellat libero asperiores earum nam nobis, culpa ratione quam perferendis
+                    esse,
+                    cupiditate neque
+                    quas!
+                </p>
+            </template>
+        </Card>
+        <Button icon="pi pi-arrow-right"/>
+
+    </div>
 </template>
