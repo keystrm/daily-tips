c38d5ec1a430183650db1b0fa1c0d5fb55ad2af5
Kasun Madushan <kasunsudarshana1@gmail.com>
2024-04-06 20:40:48 +0530

    just building basic struucture of the app

--- app.vue
+++ app.vue
@@ -1,7 +1,26 @@
 <template>
-  <div>
-    <Main/>
-  </div>
+  <NuxtLayout>
+    <div>
+      0
+    </div>
+    <div>
+      1
+      <ColorModeSwitch/>
+    </div>
+    <div class="col-span-2">
+      2
+      <Card>
+            <template #title> Simple Card </template>
+            <template #content>
+                <p class="m-0">
+                    Lorem ipsum dolor sit amet, consectetur adipisicing elit. Inventore sed consequuntur error repudiandae numquam deserunt quisquam repellat libero asperiores earum nam nobis, culpa ratione quam perferendis esse, cupiditate neque
+                    quas!
+                </p>
+            </template>
+        </Card>
+    </div>
+
+  </NuxtLayout>
 </template>
 
 <script setup lang="ts">
---
+++ layouts/default.vue
@@ -0,0 +1,6 @@
+<template>
+    <div class="grid grid-cols-2 min-h-screen bg-surface-0 dark:bg-surface-950 text-surface-950 dark:text-surface-50">
+        <slot/>
+    </div>
+
+</template>
