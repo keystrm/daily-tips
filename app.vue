d7f7459051603e75b9d612e9687e4f31fa858782
Kasun Madushan <kasunsudarshana1@gmail.com>
2024-04-06 20:57:56 +0530

    add selection dropdown

--- app.vue
+++ app.vue
@@ -1,30 +1,60 @@
 <template>
   <NuxtLayout>
-    <div>
-      0
-    </div>
-    <div>
-      1
-      <ColorModeSwitch/>
-    </div>
-    <div class="col-span-2">
-      2
+    <div class="md:max-w-2xl min-h-2xl">
+      <div>
+        <Dropdown v-model="selectedCountry" :options="countries" filter optionLabel="name"
+          placeholder="Select a Country" class="w-full md:w-14rem">
+          <template #value="slotProps">
+            <div v-if="slotProps.value" class="flex align-items-center">
+              <img :alt="slotProps.value.label"
+                src="https://primefaces.org/cdn/primevue/images/flag/flag_placeholder.png"
+                :class="`mr-2 flag flag-${slotProps.value.code.toLowerCase()}`" style="width: 18px" />
+              <div>{{ slotProps.value.name }}</div>
+            </div>
+            <span v-else>
+              {{ slotProps.placeholder }}
+            </span>
+          </template>
+          <template #option="slotProps">
+            <div class="flex align-items-center">
+              <img :alt="slotProps.option.label"
+                src="https://primefaces.org/cdn/primevue/images/flag/flag_placeholder.png"
+                :class="`mr-2 flag flag-${slotProps.option.code.toLowerCase()}`" style="width: 18px" />
+              <div>{{ slotProps.option.name }}</div>
+            </div>
+          </template>
+        </Dropdown>
+        <ColorModeSwitch />
+      </div>
       <Card>
-            <template #title> Simple Card </template>
-            <template #content>
-                <p class="m-0">
-                    Lorem ipsum dolor sit amet, consectetur adipisicing elit. Inventore sed consequuntur error repudiandae numquam deserunt quisquam repellat libero asperiores earum nam nobis, culpa ratione quam perferendis esse, cupiditate neque
-                    quas!
-                </p>
-            </template>
-        </Card>
+        <template #title> Simple Card </template>
+        <template #content>
+          <p class="m-0">
+            Lorem ipsum dolor sit amet, consectetur adipisicing elit. Inventore sed consequuntur error repudiandae
+            numquam deserunt quisquam repellat libero asperiores earum nam nobis, culpa ratione quam perferendis esse,
+            cupiditate neque
+            quas!
+          </p>
+        </template>
+      </Card>
     </div>
 
   </NuxtLayout>
 </template>
 
 <script setup lang="ts">
+const selectedCountry = ref();
+const countries = ref([
+    { name: 'Australia', code: 'AU' },
+    { name: 'Brazil', code: 'BR' },
+    { name: 'China', code: 'CN' },
+    { name: 'Egypt', code: 'EG' },
+    { name: 'France', code: 'FR' },
+    { name: 'Germany', code: 'DE' },
+    { name: 'India', code: 'IN' },
+    { name: 'Japan', code: 'JP' },
+    { name: 'Spain', code: 'ES' },
+    { name: 'United States', code: 'US' }
+]);
 </script>
-<style>
-
-</style>
+<style></style>
