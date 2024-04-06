db8daa73d6ab2f5422f0615c3ce1d63e1cefaa18
Kasun Madushan <kasunsudarshana1@gmail.com>
2024-04-06 23:49:57 +0530

    basic structure of the app
    include all ui components

--- app.vue
+++ app.vue
@@ -1,43 +1,48 @@
 <template>
   <NuxtLayout>
-    <div class="md:max-w-2xl min-h-2xl">
-      <div>
-        <Dropdown v-model="selectedCountry" :options="countries" filter optionLabel="name"
-          placeholder="Select a Country" class="w-full md:w-14rem">
-          <template #value="slotProps">
-            <div v-if="slotProps.value" class="flex align-items-center">
-              <img :alt="slotProps.value.label"
-                src="https://primefaces.org/cdn/primevue/images/flag/flag_placeholder.png"
-                :class="`mr-2 flag flag-${slotProps.value.code.toLowerCase()}`" style="width: 18px" />
-              <div>{{ slotProps.value.name }}</div>
-            </div>
-            <span v-else>
-              {{ slotProps.placeholder }}
-            </span>
-          </template>
-          <template #option="slotProps">
-            <div class="flex align-items-center">
-              <img :alt="slotProps.option.label"
-                src="https://primefaces.org/cdn/primevue/images/flag/flag_placeholder.png"
-                :class="`mr-2 flag flag-${slotProps.option.code.toLowerCase()}`" style="width: 18px" />
-              <div>{{ slotProps.option.name }}</div>
-            </div>
-          </template>
-        </Dropdown>
-        <Calendar v-model="buttondisplay" showIcon />
+    <div class="md:max-w-2xl">
+      <div class="flex flex-col md:flex-row gap-2 my-3 md:justify-between">
+        <div class="flex flex-col md:flex-row">
+          <Dropdown v-model="selectedCountry" :options="countries" filter optionLabel="name"
+            placeholder="Select a Country" class="w-full md:w-14rem">
+            <template #value="slotProps">
+              <div v-if="slotProps.value" class="flex align-items-center">
+                <img :alt="slotProps.value.label"
+                  src="https://primefaces.org/cdn/primevue/images/flag/flag_placeholder.png"
+                  :class="`mr-2 flag flag-${slotProps.value.code.toLowerCase()}`" style="width: 18px" />
+                <div>{{ slotProps.value.name }}</div>
+              </div>
+              <span v-else>
+                {{ slotProps.placeholder }}
+              </span>
+            </template>
+            <template #option="slotProps">
+              <div class="flex align-items-center">
+                <img :alt="slotProps.option.label"
+                  src="https://primefaces.org/cdn/primevue/images/flag/flag_placeholder.png"
+                  :class="`mr-2 flag flag-${slotProps.option.code.toLowerCase()}`" style="width: 18px" />
+                <div>{{ slotProps.option.name }}</div>
+              </div>
+            </template>
+          </Dropdown>
+          <Calendar v-model="buttondisplay" showIcon />
+        </div>
+
         <ColorModeSwitch />
       </div>
-      <Card>
-        <template #title> Simple Card </template>
-        <template #content>
-          <p class="m-0">
-            Lorem ipsum dolor sit amet, consectetur adipisicing elit. Inventore sed consequuntur error repudiandae
-            numquam deserunt quisquam repellat libero asperiores earum nam nobis, culpa ratione quam perferendis esse,
-            cupiditate neque
-            quas!
-          </p>
-        </template>
-      </Card>
+      <div class="">
+        <Card>
+          <template #title> Simple Card </template>
+          <template #content>
+            <p class="m-0">
+              Lorem ipsum dolor sit amet, consectetur adipisicing elit. Inventore sed consequuntur error repudiandae
+              numquam deserunt quisquam repellat libero asperiores earum nam nobis, culpa ratione quam perferendis esse,
+              cupiditate neque
+              quas!
+            </p>
+          </template>
+        </Card>
+      </div>
     </div>
 
   </NuxtLayout>
@@ -47,16 +52,16 @@
 const selectedCountry = ref();
 const buttondisplay = ref();
 const countries = ref([
-    { name: 'Australia', code: 'AU' },
-    { name: 'Brazil', code: 'BR' },
-    { name: 'China', code: 'CN' },
-    { name: 'Egypt', code: 'EG' },
-    { name: 'France', code: 'FR' },
-    { name: 'Germany', code: 'DE' },
-    { name: 'India', code: 'IN' },
-    { name: 'Japan', code: 'JP' },
-    { name: 'Spain', code: 'ES' },
-    { name: 'United States', code: 'US' }
+  { name: 'Australia', code: 'AU' },
+  { name: 'Brazil', code: 'BR' },
+  { name: 'China', code: 'CN' },
+  { name: 'Egypt', code: 'EG' },
+  { name: 'France', code: 'FR' },
+  { name: 'Germany', code: 'DE' },
+  { name: 'India', code: 'IN' },
+  { name: 'Japan', code: 'JP' },
+  { name: 'Spain', code: 'ES' },
+  { name: 'United States', code: 'US' }
 ]);
 </script>
 <style></style>
--- layouts/default.vue
+++ layouts/default.vue
@@ -1,5 +1,5 @@
 <template>
-    <div class="grid place-items-center min-h-screen bg-surface-0 dark:bg-surface-950 text-surface-950 dark:text-surface-50">
+    <div class="flex justify-center min-h-screen bg-surface-0 dark:bg-surface-950 text-surface-950 dark:text-surface-50">
         <slot/>
     </div>
 
