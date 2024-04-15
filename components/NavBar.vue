a56f95d14043d3d6e3673ec40b062ef07fe590e9
Kasun Madushan <kasunsudarshana1@gmail.com>
2024-04-15 23:32:22 +0530

    documentated explanations

--- components/NavBar.vue
+++ components/NavBar.vue
@@ -22,9 +22,9 @@ const {
 } = useContentHelpers()
 const route = useRoute()
 
-const bottomPath = navBottomLink(navigation.value)
-const dir = navDirFromPath(route.path, navigation.value)
-const page = navPageFromPath(route.path, navigation.value)
-const layout = navKeyFromPath(route.path, 'layout', navigation.value)
+const bottomPath = navBottomLink(navigation.value) //Take a navigation node and will resolve the first available path from that node.
+const dir = navDirFromPath(route.path, navigation.value) //This function will take a path and will resolve the first available navigation node from that path.
+const page = navPageFromPath(route.path, navigation.value) //This function will take a path and will resolve the first available navigation page from that path.
+const layout = navKeyFromPath(route.path, 'layout', navigation.value)//This function will take a path and will resolve a specific key from that path.
 
 </script>
