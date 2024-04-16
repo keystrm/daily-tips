72e001bf10f8f3e8e103fc3e03f39b809d47612e
Kasun Madushan <kasunsudarshana1@gmail.com>
2024-04-16 22:05:50 +0530

    refactor and fix some logic issues

--- components/ContentSlider.vue
+++ components/ContentSlider.vue
@@ -1,13 +1,13 @@
 <template>
     <div class="flex">
-        <SlideButton icon="pi pi-arrow-left" @click="navigate('previous')"/>
+        <SlideButton icon="pi pi-arrow-left" @click="navigate('previous')" />
         <Card>
             <template #title> {{ currentNav?.title }} </template>
             <template #content>
                 <slot></slot>
             </template>
         </Card>
-        <SlideButton icon="pi pi-arrow-right" @click="navigate('next')"/>
+        <SlideButton icon="pi pi-arrow-right" @click="navigate('next')" />
 
     </div>
 </template>
@@ -24,46 +24,44 @@ const { data: navigation } = await useAsyncData('navigation', () => fetchContent
 const route = useRoute()
 
 const navigationList = computed(() => {
-  const result: NavComponent[] = [];
-  const uniquePaths = new Set<string>();
+    const result: NavComponent[] = [];
+    const uniquePaths = new Set<string>();
 
-  function addItems(items: NavItem[]) {
-    items.forEach(item => {
-      const itemKey = `${item.title}|${item._path}`;
-      if (!uniquePaths.has(itemKey)) {
-        result.push({ title: item.title, _path: item._path });
-        uniquePaths.add(itemKey);
-      }
-      if (item.children && item.children.length > 0) {
-        addItems(item.children);
-      }
-    });
-  }
+    function addItems(items: NavItem[]) {
+        items.forEach(item => {
+            const itemKey = `${item.title}|${item._path}`;
+            if (!uniquePaths.has(itemKey)) {
+                result.push({ title: item.title, _path: item._path });
+                uniquePaths.add(itemKey);
+            }
+            if (item.children && item.children.length > 0) {
+                addItems(item.children);
+            }
+        });
+    }
 
-  if (navigation.value) {
-    addItems(navigation.value);
-  }
-  
-  return result;
+    if (navigation.value) {
+        addItems(navigation.value);
+    }
+
+    return result;
 });
 
-const currentNav = computed(()=>{
-    return navigationList.value.find((item)=>item._path===route.path)
+const currentNav = computed(() => {
+    return navigationList.value.find((item) => item._path === route.path)
 })
-const currentNavIndex = computed(()=>{
-    return navigationList.value.findIndex((item)=>item._path===route.path)
+const currentNavIndex = computed(() => {
+    return navigationList.value.findIndex((item) => item._path === route.path)
 })
 
-const navigate = (direction :'next'|'previous'):void => {
-    if(currentNavIndex.value && currentNavIndex.value !== -1){
-        if(direction === 'next' && currentNavIndex.value+1 < navigationList.value.length){
-            console.log(navigationList.value)
-            navigateTo(navigationList.value[currentNavIndex.value+1]._path)
+const navigate = (direction: 'next' | 'previous'): void => {
+    if (currentNavIndex.value !== -1) {
+        if (direction === 'next' && currentNavIndex.value + 1 < navigationList.value.length) {
+            navigateTo(navigationList.value[currentNavIndex.value + 1]._path)
         }
-        else if(direction === 'previous' && currentNavIndex.value - 1 >= 0){
-            navigateTo(navigationList.value[currentNavIndex.value -1]._path)
+        else if (direction === 'previous' && currentNavIndex.value - 1 >= 0) {
+            navigateTo(navigationList.value[currentNavIndex.value - 1]._path)
         }
     }
 }
-
 </script>
