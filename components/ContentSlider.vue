61785828dd581b0e36f4eaa5b58d356d7ba76692
Kasun Madushan <kasunsudarshana1@gmail.com>
2024-04-16 09:14:52 +0530

    make the complete navigation system

--- components/ContentSlider.vue
+++ components/ContentSlider.vue
@@ -16,6 +16,7 @@
 import type { NavItem } from '@nuxt/content/types';
 
 const { data: navigation } = await useAsyncData('navigation', () => fetchContentNavigation())
+const route = useRoute()
 
 const flattenArray = (data: NavItem[]): { title: string; _path: string; }[] =>{
     let result: { title: string; _path: string; }[] = [];
@@ -38,29 +39,40 @@ const flattenArray = (data: NavItem[]): { title: string; _path: string; }[] =>{
     return result;
 }
 
-const getNextIndex = (flatArray: { title: string; _path: string; }[], currentPath: string): number | null =>{
+const getNextNode = (flatArray: { title: string; _path: string; }[], currentPath: string): { title: string; _path: string; } | null =>{
     const currentIndex = flatArray.findIndex(item => item._path === currentPath);
     if (currentIndex === -1 || currentIndex + 1 >= flatArray.length) {
         return null;
     }
-    return currentIndex + 1;
+    return flatArray[currentIndex + 1];
 }
 
-const getPreviousIndex = (flatArray: { title: string; _path: string; }[], currentPath: string): number | null =>{
+const getPreviousNode = (flatArray: { title: string; _path: string; }[], currentPath: string): { title: string; _path: string; } | null =>{
     const currentIndex = flatArray.findIndex(item => item._path === currentPath);
     if (currentIndex === -1 || currentIndex - 1 < 0) {
         return null;
     }
-    return currentIndex - 1;
+    return flatArray[currentIndex - 1];
 }
 
 const nextNote = () => {
     if(navigation.value){
         const articleList = flattenArray(navigation.value)
-        console.log(articleList)
+        const nextNode = getNextNode(articleList,route.path)
+
+        if(nextNode){
+            navigateTo(nextNode._path)
+        }        
     }
 }
 const previousNote = () => {
-    
+    if(navigation.value){
+        const articleList = flattenArray(navigation.value)
+        const previousNode = getPreviousNode(articleList,route.path)
+
+        if(previousNode){
+            navigateTo(previousNode._path)
+        }        
+    }
 }
 </script>
