9f56487d38cb27d2332a0878c9ebf1df0d0c4296
Kasun Madushan <kasunsudarshana1@gmail.com>
2024-04-16 08:54:45 +0530

    make a flat array for the two button navigation

--- components/ContentSlider.vue
+++ components/ContentSlider.vue
@@ -13,10 +13,28 @@
 </template>
 
 <script setup lang="ts">
-const { data: navigation } = await useAsyncData('navigation', () => fetchContentNavigation())
-console.log(navigation.value)
-const nextNote = () => {
+import type { NavItem } from '@nuxt/content/types';
 
+const { data: navigation } = await useAsyncData('navigation', () => fetchContentNavigation())
+
+const flattenArray = (data: NavItem[]): { title: string; _path: string; }[] =>{
+    let result: { title: string; _path: string; }[] = [];
+
+    data.forEach(item => {
+        result.push({ title: item.title, _path: item._path });
+        if (item.children && item.children.length > 0) {
+            result = result.concat(flattenArray(item.children));
+        }
+    });
+
+    return result;
+}
+
+const nextNote = () => {
+    if(navigation.value){
+        const articleList = flattenArray(navigation.value)
+        console.log(articleList)
+    }
 }
 const previousNote = () => {
     
