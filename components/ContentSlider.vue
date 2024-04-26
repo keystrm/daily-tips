ac16b9b66e2ebec2b6383fcc2e160d4861417d4e
Kasun Madushan <kasunsudarshana1@gmail.com>
2024-04-26 09:33:54 +0530

    use the composable
    have to fix the errors cause of router or useasync outside the settup function

--- components/ContentSlider.vue
+++ components/ContentSlider.vue
@@ -10,63 +10,7 @@
 </template>
 
 <script setup lang="ts">
-interface NavComponent {
-    title: string,
-    _path: string,
-    author?:string,
-    category?:string,
-    publishedAt?:string
-}
 
-import type { NavItem } from '@nuxt/content/types';
+const {navigate,currentNav} = await useNavigation()
 
-const { data: navigation } = await useAsyncData('navigation', () => fetchContentNavigation())
-const route = useRoute()
-
-const includeParents = ref(false);
-
-const navigationList = computed(() => {
-    const result: NavComponent[] = [];
-    const uniquePaths = new Set();
-
-    function addItems(items: NavItem[], isChild = false) {
-        items.forEach(item => {
-            const itemKey = `${item.title}|${item._path}`;
-            if (!uniquePaths.has(itemKey)) {
-                // Conditionally add item based on the flag and whether it's a child node
-                if (item._path === "/" || includeParents.value || isChild) {
-                    result.push({ title: item.title, _path: item._path });
-                }
-                uniquePaths.add(itemKey);
-            }
-            if (item.children && item.children.length > 0) {
-                addItems(item.children, true);
-            }
-        });
-    }
-
-    if (navigation.value) {
-        addItems(navigation.value);
-    }
-    return result;
-});
-
-const currentNav = computed(() => {
-    return navigationList.value.find((item) => item._path === route.path)
-})
-const currentNavIndex = computed(() => {
-    return navigationList.value.findIndex((item) => item._path === route.path)
-})
-
-const navigate = (direction: 'next' | 'previous'): void => {
-
-    if (currentNavIndex.value !== -1) {
-        if (direction === 'next' && currentNavIndex.value + 1 < navigationList.value.length) {
-            navigateTo(navigationList.value[currentNavIndex.value + 1]._path)
-        }
-        else if (direction === 'previous' && currentNavIndex.value - 1 >= 0) {
-            navigateTo(navigationList.value[currentNavIndex.value - 1]._path)
-        }
-    }
-}
 </script>
