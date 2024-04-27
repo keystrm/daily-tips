0079e118a716994be0de0c8bf805af7d69f8d6e6
Kasun Madushan <kasunsudarshana1@gmail.com>
2024-04-27 11:22:06 +0530

    make thie filter component more stylish and show actual categories and authors in dropdowns

--- components/FilterDialog.vue
+++ components/FilterDialog.vue
@@ -1,39 +1,58 @@
 <template>
-    <Dialog v-model:visible="visible" modal 
-    header="Edit Profile" 
-    :style="{ width: '50rem' }" 
-    :breakpoints="{ '1199px': '75vw', '575px': '90vw' }"
-    id="filterDialog"
-    >
+    <Dialog v-model:visible="visible" modal header="Edit Profile" :style="{ width: '25rem' }" id="filterDialog">
         <template #header>
             <div class="inline-flex align-items-center justify-content-center gap-2">
                 <span class="font-bold white-space-nowrap">Filters</span>
             </div>
         </template>
-        <!-- <ContributionGrid/> -->
-        <Dropdown v-model="selectedCountry" :options="countries" filter optionLabel="name"
-            placeholder="Select a Country" class="w-full md:w-14rem">
-            <template #value="slotProps">
-                <div v-if="slotProps.value" class="flex align-items-center">
-                    <img :alt="slotProps.value.label"
-                        src="https://primefaces.org/cdn/primevue/images/flag/flag_placeholder.png"
-                        :class="`mr-2 flag flag-${slotProps.value.code.toLowerCase()}`" style="width: 18px" />
-                    <div>{{ slotProps.value.name }}</div>
-                </div>
-                <span v-else>
-                    {{ slotProps.placeholder }}
-                </span>
-            </template>
-            <template #option="slotProps">
-                <div class="flex align-items-center">
-                    <img :alt="slotProps.option.label"
-                        src="https://primefaces.org/cdn/primevue/images/flag/flag_placeholder.png"
-                        :class="`mr-2 flag flag-${slotProps.option.code.toLowerCase()}`" style="width: 18px" />
-                    <div>{{ slotProps.option.name }}</div>
-                </div>
-            </template>
-        </Dropdown>
-        <Calendar v-model="buttondisplay" showIcon />
+        <div class="flex flex-col space-y-4 p-4 max-w-md mx-auto">
+            <Dropdown class="w-full" v-model="selectedCountry" :options="categoriesList" filter optionLabel="name"
+                placeholder="Select a Country">
+                <template #value="slotProps">
+                    <div v-if="slotProps.value" class="flex align-items-center">
+                        <img :alt="slotProps.value.label"
+                            src="https://primefaces.org/cdn/primevue/images/flag/flag_placeholder.png"
+                            :class="`mr-2 flag flag-${slotProps.value.code.toLowerCase()}`" style="width: 18px" />
+                        <div>{{ slotProps.value.name }}</div>
+                    </div>
+                    <span v-else>
+                        {{ slotProps.placeholder }}
+                    </span>
+                </template>
+                <template #option="slotProps">
+                    <div class="flex align-items-center">
+                        <img :alt="slotProps.option.label"
+                            src="https://primefaces.org/cdn/primevue/images/flag/flag_placeholder.png"
+                            :class="`mr-2 flag flag-${slotProps.option.code.toLowerCase()}`" style="width: 18px" />
+                        <div>{{ slotProps.option.name }}</div>
+                    </div>
+                </template>
+            </Dropdown>
+            <Dropdown class="w-full" v-model="selectedAuthors" :options="authorsList" filter optionLabel="name"
+                placeholder="Select a Auhor">
+                <template #value="slotProps">
+                    <div v-if="slotProps.value" class="flex align-items-center">
+                        <img :alt="slotProps.value.label"
+                            src="https://primefaces.org/cdn/primevue/images/flag/flag_placeholder.png"
+                            :class="`mr-2 flag flag-${slotProps.value.code.toLowerCase()}`" style="width: 18px" />
+                        <div>{{ slotProps.value.name }}</div>
+                    </div>
+                    <span v-else>
+                        {{ slotProps.placeholder }}
+                    </span>
+                </template>
+                <template #option="slotProps">
+                    <div class="flex align-items-center">
+                        <img :alt="slotProps.option.label"
+                            src="https://primefaces.org/cdn/primevue/images/flag/flag_placeholder.png"
+                            :class="`mr-2 flag flag-${slotProps.option.code.toLowerCase()}`" style="width: 18px" />
+                        <div>{{ slotProps.option.name }}</div>
+                    </div>
+                </template>
+            </Dropdown>
+            <Calendar class="w-full" v-model="buttondisplay" showIcon />
+        </div>
+
         <template #footer>
             <Button label="Cancel" text severity="secondary" @click="visible = false" autofocus />
             <Button label="Apply" outlined severity="secondary" @click="visible = false" autofocus />
@@ -45,17 +64,13 @@
 <script setup lang="ts">
 const visible = defineModel({ type: Boolean });
 const selectedCountry = ref();
+const selectedAuthors = ref();
 const buttondisplay = ref();
-const countries = ref([
-    { name: 'Australia', code: 'AU' },
-    { name: 'Brazil', code: 'BR' },
-    { name: 'China', code: 'CN' },
-    { name: 'Egypt', code: 'EG' },
-    { name: 'France', code: 'FR' },
-    { name: 'Germany', code: 'DE' },
-    { name: 'India', code: 'IN' },
-    { name: 'Japan', code: 'JP' },
-    { name: 'Spain', code: 'ES' },
-    { name: 'United States', code: 'US' }
-]);
+
+const route = useRoute()
+const { authors, categories, navigate, currentNav } = await useNavigation(undefined, route)
+const categoriesList = computed(() => categories.value.map((cat) => ({ name: cat, code: "A" })))
+const authorsList = computed(() => authors.value.map((auth) => ({ name: auth, code: "A" })))
+
+
 </script>
